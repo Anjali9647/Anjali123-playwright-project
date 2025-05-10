c3a14c37a350538529973dbae2be04589271ab9d
Anjali9647 <anjalimasal0718@gmail.com>
2025-05-10 20:37:24 +0530

    Update dropdown.spec.js

--- tests/dropdown.spec.js
+++ tests/dropdown.spec.js
@@ -6,6 +6,8 @@ await page.goto("https://demoqa.com/select-menu");
 await page.locator('#withOptGroup').click();
   await page.locator('div[id^="react-select"] div:has-text("Group 1, option 1")').click();
 
+  ///this is first time try
+
 //await page.waitForSelector('.css-1uccc91-singleValue');
 //await page.setDefaultTimeout('12000');
 //await page.locator('.css-1uccc91-singleValue',{hasText:'Group 2, option 1'}).click();
